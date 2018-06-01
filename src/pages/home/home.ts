44ddb5a14c59a9901c7ae334ce2ac3ea63ae8199
EDGARLOZANO <edg283@gmail.com>
2018-05-31 20:07:48 -0600

    Creacion de motodos setup y el card para las tarjetas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,56 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+declare var Stripe;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  stripe = Stripe('pk_test_GVS0FEXJnS4DrRivhwADsV8j');
+  card: any;
+
+  token=null;
 
   constructor(public navCtrl: NavController) {
 
+  }
+   
+  ionViewDidLoad() {
+    this.setupStripe();
+  }
+
+  setupStripe(){
+    let elements = this.stripe.elements();
+    var style = {
+      base: {
+        color: '#32325d',
+        lineHeight: '24px',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+          color: '#aab7c4'
+        }
+      },
+      invalid: {
+        color: '#fa755a',
+        iconColor: '#fa755a'
+      }
+    };
+      this.card = elements.create('card', { style: style });
+
+    this.card.mount('#card-element');
+
+    this.card.addEventListener('change', event => {
+      var displayError = document.getElementById('card-errors');
+      if (event.error) {
+        displayError.textContent = event.error.message;
+      } else {
+        displayError.textContent = '';
+      }
+    });
+
+    
   }
 
 }
